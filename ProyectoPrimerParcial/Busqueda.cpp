d078f8c517e70f427aee7ee390ee925a210c49e1
Jyaru Hernandez <jyaru1110@gmail.com>
2023-02-20 19:17:13 -0600

    busqueda primero en profundidad

--- ProyectoPrimerParcial/Busqueda.cpp
+++ ProyectoPrimerParcial/Busqueda.cpp
@@ -131,3 +131,41 @@ bool Busqueda::busqueda_bidireccional(string nodo_inicio, string nodo_final, int
     }
     return false;
 }
+
+
+bool Busqueda::busqueda_primero_en_profundidad(string nodo_inicio, string nodo_final, int& nodo_encontrado)
+{
+    stack<int> pila;
+    Arbol arbol_de_busqueda;
+    Nodo raiz_nodo;
+    if(!grafo.devuelve_informacion_de_un_nodo(nodo_inicio, raiz_nodo)) return false;
+    Nodo_informacion raiz;
+    if(!arbol_de_busqueda.devuelve_informacion_de_un_vertice_grafo_no_dirigido(nodo_inicio, -1, raiz_nodo, raiz)) return false;
+    arbol_de_busqueda.crea_arbol(raiz);
+    pila.push(0);
+    while(!pila.empty())
+    {
+        int nodo_actual = pila.top();
+        if(arbol_de_busqueda.devuelve_nombre_de_un_nodo(nodo_actual) == nodo_final)
+        {
+            nodo_encontrado = nodo_actual;
+            return true;
+        }
+        pila.pop();
+        vector<Enlace> vecinos = grafo.devuelve_vecinos_de_un_nodo(arbol_de_busqueda.devuelve_nombre_de_un_nodo(nodo_actual));
+        for(auto i = vecinos.begin(); i != vecinos.end(); i++)
+        {
+            if(!arbol_de_busqueda.esta_un_nodo_en_ancestros(nodo_actual, i->nombre)) continue;
+            vecinos.erase(i);
+            i--;
+        }
+        for(auto i = vecinos.begin(); i != vecinos.end(); i++)
+        {
+            grafo.devuelve_informacion_de_un_nodo(i->nombre, raiz_nodo);
+            arbol_de_busqueda.devuelve_informacion_de_un_vertice_grafo_no_dirigido(i->nombre, nodo_actual, raiz_nodo, raiz);
+            arbol_de_busqueda.agrega_hijo_a_un_nodo(nodo_actual, raiz);
+            pila.push(arbol_de_busqueda.devuelve_tamano_del_arbol() - 1);
+        }
+    }
+    return false;
+}
