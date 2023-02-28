c9958d505596b6fbd317e63cfbb5839213e74604
Jyaru Hernandez <jyaru1110@gmail.com>
2023-02-27 21:00:25 -0600

    Busqueda en profundidad limita probada

--- ProyectoPrimerParcial/Busqueda.cpp
+++ ProyectoPrimerParcial/Busqueda.cpp
@@ -149,21 +149,16 @@ bool Busqueda::busqueda_primero_en_profundidad(string nodo_inicio, string nodo_f
     return false;
 }
 
-
-/*bool Busqueda::busqueda_primero_en_profundidad_limitada(string nodo_inicio, string nodo_final, int& nodo_encontrado, int profundidad_limite)
+bool Busqueda::busqueda_primero_en_profundidad_limitada(string nodo_inicio, string nodo_final, int& nodo_encontrado, int limite)
 {
     stack<int> pila;
     Arbol arbol_de_busqueda;
     Nodo raiz_nodo;
-    if(!grafo.devuelve_informacion_de_un_nodo(nodo_inicio, raiz_nodo)) return false;
-
     Nodo_informacion raiz;
-
+    if(!grafo.devuelve_informacion_de_un_nodo(nodo_inicio, raiz_nodo)) return false;
     arbol_de_busqueda.devuelve_informacion_de_un_vertice_grafo_no_dirigido(nodo_inicio, -1, raiz_nodo, raiz);
-
     arbol_de_busqueda.crea_arbol(raiz);
     pila.push(0);
-
     while(!pila.empty())
     {
         int nodo_actual = pila.top();
@@ -174,29 +169,20 @@ bool Busqueda::busqueda_primero_en_profundidad(string nodo_inicio, string nodo_f
             return true;
         }
         pila.pop();
-
-        //solo busca vecinos si la profundidad del grafo es menor al limite
-        vector<Enlace> vecinos;
-        vecinos.clear();
-        if(arbol_de_busqueda.devuelve_profundidad_del_nodo(nodo_actual)<=profundidad_limite)
+        unordered_map<string, float> vecinos;
+        if(arbol_de_busqueda.devuelve_profundidad_del_nodo(arbol_de_busqueda.devuelve_tamano_del_arbol() - 1) <= limite)
             vecinos = grafo.devuelve_vecinos_de_un_nodo(arbol_de_busqueda.devuelve_nombre_de_un_nodo(nodo_actual));
-
         for(auto i = vecinos.begin(); i != vecinos.end(); i++)
         {
-            if(!arbol_de_busqueda.esta_un_nodo_en_ancestros(nodo_actual, i->nombre)) continue;
-            vecinos.erase(i);
-            i--;
-        }
-        for(auto i = vecinos.begin(); i != vecinos.end(); i++)
-        {
-            grafo.devuelve_informacion_de_un_nodo(i->nombre, raiz_nodo);
-            arbol_de_busqueda.devuelve_informacion_de_un_vertice_grafo_no_dirigido(i->nombre, nodo_actual, raiz_nodo, raiz);
+            if(arbol_de_busqueda.esta_un_nodo_en_ancestros(nodo_actual, i->first)) continue;
+            grafo.devuelve_informacion_de_un_nodo(i->first, raiz_nodo);
+            arbol_de_busqueda.devuelve_informacion_de_un_vertice_grafo_no_dirigido(i->first, nodo_actual, raiz_nodo, raiz);
             arbol_de_busqueda.agrega_hijo_a_un_nodo(nodo_actual, raiz);
             pila.push(arbol_de_busqueda.devuelve_tamano_del_arbol() - 1);
         }
     }
-    return false;    
-}*/
+    return false;
+}
 
 /*bool Busqueda::busqueda_primero_en_profundidad_iterativa(string nodo_inicio, string nodo_final, int& nodo_encontrado,int primera_profundidad,int incremento_profundidad)
 {
