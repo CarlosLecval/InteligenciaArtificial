0e25b81009d60c11722d0bb58860fd11dbb71aac
Jyaru Hernandez <jyaru1110@gmail.com>
2023-02-28 23:27:59 -0600

    Pequenas mejoras a branch and bound

--- ProyectoPrimerParcial/Busqueda.cpp
+++ ProyectoPrimerParcial/Busqueda.cpp
@@ -343,13 +343,10 @@ bool Busqueda::branch_and_bound(string nodo_inicio, string nodo_final, int& nodo
             {
                 mejor_solucion = nodo_actual;
             }
-            //descarta los nodos que no pueden ser mejores soluciones
-            if(mejor_solucion != -1)
-                while(!agenda.empty() && arbol_de_busqueda.devuelve_costo_acumulado_del_nodo(agenda[0]) >= arbol_de_busqueda.devuelve_costo_acumulado_del_nodo(mejor_solucion))
-                {
-                    agenda.erase(agenda.begin());
-                }
-            //si no hay mas nodos en la agenda, termina el algoritmo
+            while(!agenda.empty() && arbol_de_busqueda.devuelve_costo_acumulado_del_nodo(agenda[agenda.size()-1]) >= arbol_de_busqueda.devuelve_costo_acumulado_del_nodo(mejor_solucion))
+            {
+                agenda.erase(agenda.end());
+            }
             if(agenda.empty()){
                 arbol = arbol_de_busqueda;
                 nodo_encontrado = mejor_solucion;
@@ -365,8 +362,10 @@ bool Busqueda::branch_and_bound(string nodo_inicio, string nodo_final, int& nodo
             grafo.devuelve_informacion_de_un_nodo(i->first, raiz_nodo);
             arbol_de_busqueda.devuelve_informacion_de_un_vertice_grafo_no_dirigido(i->first, nodo_actual, raiz_nodo, raiz);
             arbol_de_busqueda.agrega_hijo_a_un_nodo(nodo_actual, raiz);
-            agenda.push_back(arbol_de_busqueda.devuelve_tamano_del_arbol() - 1);
+            if(arbol_de_busqueda.devuelve_costo_acumulado_del_nodo(arbol_de_busqueda.devuelve_tamano_del_arbol() - 1) < arbol_de_busqueda.devuelve_costo_acumulado_del_nodo(mejor_solucion) || mejor_solucion == -1)
+                agenda.push_back(arbol_de_busqueda.devuelve_tamano_del_arbol() - 1);
         }
+        ordena_por_costo_acumulado(agenda, arbol_de_busqueda,agenda.size());
     }
     return false;
 }
